fad99dd4df397ce03acb86441fd7c432e63b279c
iblinfotech <dongavaibhav11@gmail.com>
2020-10-27 10:31:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyAdapter myAdapter;
+    private RecyclerView myRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RestApi service = Global.getClient().create(RestApi.class);
+
+        Call<List<RetroUsers>> call = service.getAllUsers();
+        call.enqueue(new Callback<List<RetroUsers>>() {
+
+            @Override
+            public void onResponse(Call<List<RetroUsers>> call, Response<List<RetroUsers>> response) {
+                loadDataList(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<RetroUsers>> call, Throwable throwable) {
+                Toast.makeText(MainActivity.this, "Unable to load users", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadDataList(List<RetroUsers> usersList) {
+        myRecyclerView = findViewById(R.id.rvView);
+        myAdapter = new MyAdapter(usersList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        myRecyclerView.setLayoutManager(layoutManager);
+        myRecyclerView.setAdapter(myAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/testing/RetroUsers.java
@@ -0,0 +1,23 @@
+package com.example.testing;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RetroUsers {
+    @SerializedName("name")
+    private String name;
+
+    public RetroUsers(String name) {
+        this.name = name;
+
+    }
+
+//Retrieve the data using setter/getter methods//
+
+    public String getUser() {
+        return name;
+    }
+
+    public void setUser(String name) {
+        this.name = name;
+    }
+}
